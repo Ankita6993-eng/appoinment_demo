0a9922f7aa2c2c1bd2f03f5fdd75956f9ecf3071
mac <mac@macs-iMac.local>
2021-06-14 17:04:07 +0530

    first commit

--- src/app/appoinment/appoinment.component.ts
+++ src/app/appoinment/appoinment.component.ts
@@ -22,31 +22,42 @@ export class AppoinmentComponent implements OnInit {
 
   ngOnInit(): void {
     this.appoinmentform=this.fb.group({
+     // day:this.day,
       schedule:new FormArray([])
     });
     
 
     for(let hour = 9; hour < 22; hour++) {  
       for(let mins=0 ;mins < 60; mins++){
-      this.start_hours.push(moment({ hour,minutes:mins }).format('h:mm A'));
-        }
-  }
+         this.start_hours.push(moment({ hour,minutes:mins }).format('H:mm'));
+      }
+    }
   
   }
   createschedule():FormGroup|any{
     return this.fb.group({
       name:[''],
-      start_time:[''],
+      start_time:this.patchDynamicFormBlockValue(),
       end_time:['']
     })
     
   }
   onselect(val:any){
     var selectedOptionIndex
-    console.log(parseInt(val))
+    console.log(val)
         let index = parseInt(val) + 30;
-        console.log('ew',index)
+       // console.log('ew',val)
         this.end_hours = this.start_hours.slice(index);
+
+    //console.log('th1', this.end_hours);
+    
+  //   if(this.start_hours > this.end_hours){
+  //    let ind=val+15;
+  //    this.start_hours=this.end_hours.slice(ind)
+  // }
+    
+
+
   }
 
   getControls() {
@@ -58,12 +69,44 @@ export class AppoinmentComponent implements OnInit {
   addschedule(){
     this.schedule=this.appoinmentform.get('schedule') as FormArray;
     this.schedule.push(this.createschedule());
+    console.log('this.schedule', this.schedule);
+   this.isdiasplay=true;
+    //this.appoinmentform.setControl('start_time', this.fb.array(this.appoinmentform.start_time || []));
   
+    // var findFirstInd = this.schedule.indexOf();
+    // console.log('index', findFirstInd );
+    
+    //((this.schedule.get('controls') as FormArray).at(index) as FormGroup).get('start_time').patchValue(schedule.start_time);
+    for (let i = 0; i < this.schedule.length; i++) {
+      console.log(this.schedule.at(i).value);
+      console.log('', );
+      
+      // var findFirstInd = this.schedule.indexOf(i);
+      // console.log('dsfsz', findFirstInd);
+      //this.schedule.get('start_time')[i]
+      //this.appoinmentform.get('start_time')[i].patchValue(this.schedule.get('end_time')[i]+15)
+    //  this.schedule.at(i).get('start_time').patchValue(this.schedule.at(i).get('end_time')[i]+15);
+      //this.schedule.controls.start_time.value [i].controls['start_time'].patchValue(this.schedule.at(i).get('end_time')[i]+15)
+         //console.log('thi', this.schedule[i].value);
+        
+        // this.schedule.get('start_time')[i].patchValue();
+        // this.schedule.get('start_time')[i]=this.schedule.get('end_time')[i]+15;
+      //console.log('ds', this.schedule.get('start_time'));
+      
+    }
   }
+
+   patchDynamicFormBlockValue(){
+     for (let i = 0; i < this.schedule.length; i++) {
+    console.log(this.schedule.at(i).value);
+    console.log('', );
+   }
+  }
+  
 
   onclick(){
     console.log('', this.appoinmentform.value);
-    this.isdiasplay=false;
+  
     //JSON.parse(this.appoinmentform.value);
     //JSON.parse(JSON.stringify(this.appoinmentform.value))
      //this.appoinmentform.value 
@@ -72,6 +115,7 @@ export class AppoinmentComponent implements OnInit {
   deleteScheduleGroup(index: number) {
     const add = this.appoinmentform.get('schedule') as FormArray;
     add.removeAt(index)
+    this.isdiasplay=false;
   }
 
 }
